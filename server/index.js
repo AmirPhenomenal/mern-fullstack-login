e6209d6afb01db2e09549dff5f14445cbe883478
AmirPhenomenal <ab.beigi1@gmail.com>
2021-11-08 19:36:30 +0330

    Adding Folder Structure For FrontEnd
    Adding Folder Structure For Redux And API Calls To Backend And Constants

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
+//For Using Env File
 import dotenv from 'dotenv';
-
+//Routes
 import userRoutes from "./routes/users.js";
 
-
 dotenv.config();
+
 const app = express();
 //Setting Limite For Datas Coming To Backend
 app.use(bodyParser.json({ limit: "5mb", extended: true }));
@@ -15,8 +16,9 @@ app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 
 app.use(cors());
 app.use("/user", userRoutes);
-
+//Setting Server Port
 const PORT = process.env.PORT || 5000;
+//Connecting To Database
 mongoose
     .connect(process.env.CONNECTION_URL, {
         useNewUrlParser: true,
