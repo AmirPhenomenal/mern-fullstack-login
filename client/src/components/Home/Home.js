f3d28422a23b186a7cd48945ac8adfd6443c212a
AmirPhenomenal <ab.beigi1@gmail.com>
2021-11-09 12:51:02 +0330

    Added Loged In Logic In Home Page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,16 +6,23 @@ import useStyles from "./styles";
 const Home = () => {
     //StyleSheet
     const classes = useStyles();
-
+    const user = JSON.parse(localStorage.getItem('profile'));
+    const isSingedIn = user?.result?.name;
+    // {/* Website Main Page Here  */}
     return (
         // Grow For Animation
         <Grow in>
-            <Container component="main" maxWidth="sm"  >
-                {/* Website Main Page Here  */}
+            <Container component="main" maxWidth="sm" >
                 <Paper elevation={3}>
-                    <Typography variant="h2" align="center" color="primary">
-                    Main WebSite Page 
+                    {isSingedIn ? (
+                    <Typography variant="h4" align="center" color="primary">
+                    You Can See This If You Are Loged In .
                     </Typography>
+                    ): (
+                    <Typography variant="h4" align="center" color="primary">
+                    You Can See This If You Are Not Loged In .
+                    </Typography>
+                    ) }
                 </Paper>
 
 
