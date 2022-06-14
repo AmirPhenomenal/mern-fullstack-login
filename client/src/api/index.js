02bb48e9d1cf247bec7a044ea68583d5d38595fb
AmirPhenomenal <50031911+AmirPhenomenal@users.noreply.github.com>
2022-06-14 20:24:06 +0430

    Fix

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "BackEnd URL" });
+const API = axios.create({ baseURL: "http://localhost:5000" });
 API.interceptors.request.use((req) => {
-    if (localStorage.getItem('profile')) {
-        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
-    }
-    return req;
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
+  return req;
 });
 export const signIn = (formData) => API.post("/user/signin", formData);
 export const signUp = (formData) => API.post("/user/signup", formData);
